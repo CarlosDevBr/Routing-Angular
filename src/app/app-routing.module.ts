4b097d4b28fecef145c505bf013d2d8014f7530f
CarlosDevBr <cahzerohenrique@gmail.com>
2022-03-23 11:14:25 -0300

    9.A: Implements Bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
-import { Error404Component } from './error404/error404.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { SobreComponent } from './sobre/sobre.component';
 
-const routes: Routes = [
-  {path: 'inicio', component: InicioComponent},
-  {path: 'sobre', component: SobreComponent, canActivate:[AuthGuard]},
-  {path: '', redirectTo:'inicio', pathMatch:'full'},
-  {path: '**', component: Error404Component}
-];
+const routes: Routes = [ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './inicio/inicio.component';
-import { SobreComponent } from './sobre/sobre.component';
-import { Error404Component } from './error404/error404.component';
 import { AuthGuard } from './auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent,
-    SobreComponent,
-    Error404Component
   ],
   imports: [
     BrowserModule,
